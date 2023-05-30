fa8a9c4e918f63212142952900879e53dd798735
joaquin <jfloresp@claro.cl>
2023-05-29 21:46:32 -0400

    terminado
    ok

--- Trabajo presupuestoapp/script.js
+++ Trabajo presupuestoapp/script.js
@@ -1,70 +1,54 @@
-// Variables globales
 let budget = 0;
 let expenses = [];
 
-// Función para calcular el presupuesto
 function calculateBudget() {
-  const budgetInput = document.getElementById('budgetInput');
+  let budgetInput = document.getElementById('budgetInput');
   budget = parseFloat(budgetInput.value);
   document.getElementById('budget').textContent = `Presupuesto: ${budget}`;
   budgetInput.value = '';
 }
 
-// Función para añadir un gasto
 function addExpense() {
-  const expenseNameInput = document.getElementById('expenseNameInput');
-  const expenseAmountInput = document.getElementById('expenseAmountInput');
+ let expenseNameInput = document.getElementById('expenseNameInput');
+  let expenseAmountInput = document.getElementById('expenseAmountInput');
   
-  const expenseName = expenseNameInput.value;
-  const expenseAmount = parseFloat(expenseAmountInput.value);
+ let expenseName = expenseNameInput.value;
+  let expenseAmount = parseFloat(expenseAmountInput.value);
   
   if (expenseName && expenseAmount) {
     expenses.push({ name: expenseName, amount: expenseAmount });
     
-    // Mostrar el gasto en la lista
-    const expenseItems = document.getElementById('expenseItems');
-    const li = document.createElement('li');
+    let expenseItems = document.getElementById('expenseItems');
+    let li = document.createElement('li');
     li.innerHTML = `${expenseName}: ${expenseAmount} <button class="deleteExpenseButton">Eliminar</button>`;
     expenseItems.appendChild(li);
     
-    // Actualizar los totales y el saldo
-    const totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-    const balance = budget - totalExpenses;
+    let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
+    let balance = budget - totalExpenses;
     document.getElementById('expenses').textContent = `Gastos: ${totalExpenses}`;
     document.getElementById('balance').textContent = `Saldo: ${balance}`;
-    
-    // Limpiar los campos de entrada
     expenseNameInput.value = '';
     expenseAmountInput.value = '';
     
-    // Agregar el listener para el botón de eliminación del gasto
-    const deleteButtons = document.getElementsByClassName('deleteExpenseButton');
+    let deleteButtons = document.getElementsByClassName('deleteExpenseButton');
     for (let i = 0; i < deleteButtons.length; i++) {
       deleteButtons[i].addEventListener('click', deleteExpense);
     }
   }
 }
-
-// Función para eliminar un gasto
 function deleteExpense(event) {
-  const listItem = event.target.parentElement;
-  const expenseText = listItem.textContent;
-  const expenseName = expenseText.split(':')[0].trim();
-  const expenseAmount = parseFloat(expenseText.split(':')[1]);
+  let listItem = event.target.parentElement;
+  let expenseText = listItem.textContent;
+  let expenseName = expenseText.split(':')[0].trim();
+  let expenseAmount = parseFloat(expenseText.split(':')[1]);
+    expenses = expenses.filter(expense => expense.name !== expenseName || expense.amount !== expenseAmount);
+    listItem.remove();
   
-  // Eliminar el gasto del arreglo
-  expenses = expenses.filter(expense => expense.name !== expenseName || expense.amount !== expenseAmount);
-  
-  // Eliminar el gasto de la lista
-  listItem.remove();
-  
-  // Actualizar los totales y el saldo
-  const totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-  const balance = budget - totalExpenses;
+  let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
+  let balance = budget - totalExpenses;
   document.getElementById('expenses').textContent = `Gastos: ${totalExpenses}`;
   document.getElementById('balance').textContent = `Saldo: ${balance}`;
 }
 
-// Event listeners
 document.getElementById('calculateButton').addEventListener('click', calculateBudget);
 document.getElementById('addExpenseButton').addEventListener('click', addExpense);
